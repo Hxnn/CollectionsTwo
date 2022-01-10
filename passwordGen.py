eefc780d682e7a40015b4d3e978098bc380136ad
Hinn <64025546+TheHinn@users.noreply.github.com>
2022-01-10 11:06:42 +0100

    Added Password Generator

---
+++ passwordGen.py
@@ -0,0 +1,19 @@
+import random
+
+choice1 = [random.choice('abcdefghijklmnopqrstuvwxyz') for q in range(2)]
+choice2 = [random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(random.randint(1, 3))]
+choice3 = [random.choice('1234567890') for o in range(random.randint(4, 7))]
+choice4 = [random.choice('!@#$%^&*()') for y in range(3)]
+
+choice1n2 = choice1 + choice2
+random.shuffle(choice1n2)
+choiceALL = choice1 + choice2 + choice3 + choice4
+random.shuffle(choiceALL)
+choiceTotall = choice1n2 + choiceALL
+choiceSUM = 24 - len(choiceTotall)
+choiceADD = [random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(choiceSUM)]
+if len(choiceTotall) < 24:
+    print(''.join(choiceTotall + choiceADD))
+    print('Totale Characters:', len(choiceTotall + choiceADD))
+else:
+    print(choiceTotall)
